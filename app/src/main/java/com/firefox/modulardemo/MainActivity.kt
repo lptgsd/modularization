c9674e470521d7d7016678626b6086abe8eccd3c
jiangshuai321 <838318633@qq.com>
2021-09-17 11:14:44 +0800

    添加远程库 配置

--- app/src/main/java/com/firefox/modulardemo/MainActivity.kt
+++ app/src/main/java/com/firefox/modulardemo/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.firefox.common.base.BaseActivity
+import com.xxx.library.HomeFragment
+import com.xxx.library.TestTools
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.flContainer,HomeFragment()).commit()
     }
 
     fun test(view: android.view.View) {
